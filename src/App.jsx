ae08ad17ddf4a6078aecb15d2728919edc13fb05
SagarLonkar-18 <sagarlonkar75@gmail.com>
2025-05-09 15:36:11 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AppLayout from './layout/AppLayout'
+import Home from './pages/Home'
+import Categories from './pages/Categories'
+import Search from './pages/Search'
+import Favourites from './pages/Favourites'
+import SingleGif from './pages/SingleGif'
+
+// homepage
+// categories
+// search
+// single gif
+// favourites
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/:category',
+        element: <Categories/>
+      },
+      {
+        path: '/search/:query',
+        element: <Search/>
+      },
+      {
+        path: '/:type/:slug',
+        element: <SingleGif/>
+      },
+      {
+        path: '/favourites',
+        element: <Favourites/>
+      },
+    ]
+  }
+])
 
 const App = () => {
     return (
-      <div className='bg-red-500'>App</div>
+      <RouterProvider router={router}/>
     )
 }
 
---
+++ src/pages/SingleGif.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleGif = () => {
+    return (
+        <div>SingleGif</div>
+    )
+}
+
+export default SingleGif
